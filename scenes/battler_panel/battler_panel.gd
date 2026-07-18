2507e9a6ea8f406ce6d1e3a0f8a0d11efbcf5167
Julian L <julian.lawrance@gmail.com>
2026-07-19 00:17:25 +0100

    Remove mana points usage from battles
    Squashed commit of the following:
    
    commit dabbdd97500d8fceeb7c4b5c7134431400c43e0d
    Author: Julian L <julian.lawrance@gmail.com>
    Date:   Sat Jul 18 23:50:26 2026 +0100
    
        Remove mana text from battler panel
    
    commit 9ed8c49a61afc010c2d5116401948f1aa58b0a0e
    Author: Julian L <julian.lawrance@gmail.com>
    Date:   Sat Jul 18 23:31:33 2026 +0100
    
        Remove mana requirements from relevant items

--- scenes/battler_panel/battler_panel.gd
+++ scenes/battler_panel/battler_panel.gd
@@ -17,9 +17,6 @@ var name_label: Label = %NameLabel
 @onready
 var hp_label: Label = %HPLabel
 
-@onready
-var mp_label: Label = %MPLabel
-
 func _ready() -> void:
 	_refresh()
 
@@ -30,22 +27,8 @@ func _refresh() -> void:
 	if hp_label:
 		hp_label.text = _compute_hp_text()
 
-	if mp_label:
-		mp_label.visible = not battler or battler.has_mana_pool()
-		mp_label.text = _compute_mp_text()
-
 func _compute_hp_text() -> String:
 	if not battler:
 		return "HP: N/A"
 
 	return "HP: %d/%d" % [battler.health.current_health, battler.health.max_health]
-
-func _compute_mp_text() -> String:
-	if not battler:
-		return "MP: N/A"
-
-	if not battler.has_mana_pool():
-		return "MP: N/A"
-
-	var mana_pool := battler.get_mana_pool()
-	return "MP: %d/%d" % [mana_pool.current_points, mana_pool.max_points]
