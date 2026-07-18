2161e797fce93a32b22f3421350aa7e4d019ee1b
Julian L <julian.lawrance@gmail.com>
2026-07-18 18:01:10 +0100

    Make cave map smaller
    Squashed commit of the following:
    
    commit 350dd57a09d18c65bafc80d4e6e9da1bce552496
    Author: Julian L <julian.lawrance@gmail.com>
    Date:   Sat Jul 18 17:52:09 2026 +0100
    
        Reduce size of cave map
    
    commit a68a9b091d7f15b89e12d0dfbfb6788d7919065b
    Author: Julian L <julian.lawrance@gmail.com>
    Date:   Sat Jul 18 17:44:31 2026 +0100
    
        Only trigger MP3 boo dialogue once

--- scenes/maps/cave_map.gd
+++ scenes/maps/cave_map.gd
@@ -6,7 +6,14 @@ extends BaseMap
 ## Start from a given title when using balloon as a [Node] in a scene.
 @export var start_from_title: String = ""
 
+@onready
+var mp3_player_boo_area: Area2D = %Mp3PlayerBooArea
 
-func _on_mp_3_player_boo_area_body_entered(body: Node2D) -> void:
+func _ready() -> void:
+	SignalHelper.once(
+		mp3_player_boo_area.body_entered,
+		_on_mp3_player_boo_area_body_entered)
+
+func _on_mp3_player_boo_area_body_entered(body: Node2D) -> void:
 	if body is Player:
 		DialogueManager.show_dialogue_balloon(dialogue_resource, start_from_title)
