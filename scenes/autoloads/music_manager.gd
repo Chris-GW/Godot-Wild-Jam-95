a04ec302bf2ba7cd258c7ad7c61c401dc854a7bb
Chris-GW <Chris-GW@users.noreply.github.com>
2026-07-19 16:40:39 +0200

    fade out encounter music

--- scenes/autoloads/music_manager.gd
+++ scenes/autoloads/music_manager.gd
@@ -14,7 +14,12 @@ func play_encounter_music(stream: AudioStream) -> void:
 		_audio_player.stream = stream
 		_audio_player.play()
 
-func stop_music() -> void:
+func stop_music(music_fade_duration := 1.5) -> void:
 	if _audio_player and _audio_player.playing:
+		var tween = create_tween()
+		tween.tween_property(_audio_player, "volume_db", -60.0, music_fade_duration)
+		
+		await tween.finished
 		_audio_player.stream = null
 		_audio_player.stop()
+		_audio_player.volume_db = 0.0
